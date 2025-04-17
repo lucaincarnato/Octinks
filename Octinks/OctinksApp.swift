27cf8153ad0a15dac3ebd1da9f5a5ebea7ea37e1
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-04-17 16:54:56 +0200

    Initial Commit

---
+++ Octinks/OctinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctinksApp.swift
+//  Octinks
+//
+//  Created by Luca Maria Incarnato on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct OctinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
